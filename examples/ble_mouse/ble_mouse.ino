c1e2a01bbb123accbd6e22c44abda3b7fab4ac9b
tcoppex <tcoppex@protonmail.com>
2022-03-23 20:26:31 +0100

    Cleanup & fix type issue (uint64 to uint32).

--- examples/ble_mouse/ble_mouse.ino
+++ examples/ble_mouse/ble_mouse.ino
@@ -3,16 +3,23 @@
 ///
 ///   created: 2020-05
 ///   
-///  Create a wireless Human Interface Device (HID) using the Bluetooth Low-Energy (BLE) 
-///  HID-over-GATT Profile (HOGP) on a mbed stack (Arduino nano 33 BLE).
+///  Simulate a mouse using an analogic joystick on an Arduino nano 33 ble.
+///
+///  Pins :  
+///     * A7 : joystick X.
+///     * A6 : joystick Y.
+///     * D2 : joystick button.
 ///
 
 #include "Nano33BleHID.h"
+
 #include "AnalogJoystick.h"
 #include "signal_utils.h"
 
-#define DEMO_ENABLE_RANDOM_INPUT        1
-#define DEMO_DURATION_MS                4200
+/* -------------------------------------------------------------------------- */
+
+static constexpr bool kEnableDemoMode = true;
+static constexpr uint32_t kDemoModeTotalRuntime = 4200;
 
 /* -------------------------------------------------------------------------- */
 
@@ -20,11 +27,11 @@ Nano33BleMouse bleMouse("nano33BLE Mouse");
 
 // Analog Joystick wrapper, used to simulate a mouse.
 AnalogJoystick gJoystick(A7, A6, 2);
-static const float kJoystickSensibility = 0.125f;
+static const float kJoystickSensibility = 0.10f;
 
 // Builtin LED animation delays when disconnect. 
-static const int kLedBeaconDelayMilliseconds = 1250;
-static const int kLedErrorDelayMilliseconds = kLedBeaconDelayMilliseconds / 10;
+static const uint32_t kLedBeaconDelayMilliseconds = 1250;
+static const uint32_t kLedErrorDelayMilliseconds = kLedBeaconDelayMilliseconds / 10;
 
 // Builtin LED intensity when connected.
 static const int kLedConnectedIntensity = 30;
@@ -36,56 +43,71 @@ void setup()
   // General setup.
   pinMode(LED_BUILTIN, OUTPUT);
   gJoystick.initialize();
-
+  Serial.begin(9600); // serial don't work before the loop thread..
+  
   // Initialize both BLE and the HID.
   bleMouse.initialize();
-
+  
   // Launch the event queue that will manage both BLE events and the loop. 
   // After this call the main thread will be halted.
-  MbedBleHID_RunEventThread();
+  MbedBleHID_RunEventThread(); 
 }
 
 void loop()
 {
   // When disconnected, we animate the builtin LED to indicate the device state.
-  if (bleMouse.connected() == false) {
+  if (bleMouse.disconnected()) {
     animateLED(LED_BUILTIN, (bleMouse.has_error()) ? kLedErrorDelayMilliseconds 
                                                    : kLedBeaconDelayMilliseconds);
     return;
   }
+//  printf("connection time : %d\n", bleMouse.connection_time());
 
   // When connected, we slightly dim the builtin LED.
   analogWrite(LED_BUILTIN, kLedConnectedIntensity);
-  
-  // Read the analog joystick inputs.
-  gJoystick.update();
-  float fx = kJoystickSensibility * gJoystick.x();
-  float fy = kJoystickSensibility * gJoystick.y();
-  auto buttons = gJoystick.button() ? HIDMouseService::BUTTON_LEFT 
-                                    : HIDMouseService::BUTTON_NONE;
 
-  // When demo mode is enabled we bypass the captured values 
-  // to output random motion for a few seconds instead.
-#if DEMO_ENABLE_RANDOM_INPUT
-  bool const bPlayDemo = true;// && (bleMouse.connection_time() < DEMO_DURATION_MS);
-  if (bPlayDemo)
+  // --------
+
+  // Mouse states.
+  float fx = 0.0f;
+  float fy = 0.0f;
+  HIDMouseService::Button buttons = HIDMouseService::BUTTON_NONE;
+
+  if constexpr(kEnableDemoMode)
   {
-    fx = kJoystickSensibility * randf(-1.0f, 1.0f);
-    fy = kJoystickSensibility * randf(-1.0f, 1.0f);
-    buttons = HIDMouseService::BUTTON_NONE;
+    // printf("connection time : %d %d\n", bleMouse.connection_time(), kDemoModeTotalRuntime);
+  
+    // When demo mode is enabled we output random motion valuesfor a few seconds.
+    bool const bRunDemo = true && (bleMouse.connection_time() < kDemoModeTotalRuntime);
+    if (bRunDemo)
+    {
+      fx = kJoystickSensibility * randf(-1.0f, 1.0f);
+      fy = kJoystickSensibility * randf(-1.0f, 1.0f);
+    } else {
+      printf("Demo has ended.\n");
+    }
   }
   else
   {
-    fx = 0.0f;
-    fy = 0.0f;
+    // Otherwhise we read the analog joystick inputs.
+    gJoystick.update();
+    fx = kJoystickSensibility * gJoystick.x();
+    fy = kJoystickSensibility * gJoystick.y();
+    buttons = gJoystick.button() ? HIDMouseService::BUTTON_LEFT 
+                                 : HIDMouseService::BUTTON_NONE
+                                 ;
   }
-#endif
+
+  // --------
+
+  auto *mouse = bleMouse.hid();
 
   // Update the HID report.
-  auto *mouse = bleMouse.hid();
   mouse->motion(fx, fy);
   mouse->button(buttons);
-  mouse->SendReport();
+
+  // Send report to the ble server.
+  mouse->sendReport();
 }
 
 /* -------------------------------------------------------------------------- */
